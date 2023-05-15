c0cdf5d9f18e613f989dc1a18b6d29e7473b8f8e
Fuzzy <fuzzphorescence@gmail.com>
2023-05-15 18:56:02 +0530

    Solve Day 1 Pt 2 puzzle

--- 2022/Night1.cpp
+++ 2022/Night1.cpp
@@ -6,27 +6,51 @@ int main() {
 	int great1 = 0, great2 = 0, great3 = 0, sum = 0;
     string tempstr;
     getline(cin, tempstr);
+
     while(cin) {
         if (tempstr == "") {
-		if (sum > great1) {
-            int temp1 = great1;
-            int temp2 = great2;
-            great1 = sum;
-            great2 = temp1;
-            great3 = temp2;
-        }
-		sum = 0;
-	}
+            if (sum > great3) {
+                if (sum > great1) {
+                    int temp1 = great1;
+                    int temp2 = great2;
+                    great1 = sum;
+                    great2 = temp1;
+                    great3 = temp2;
+                }
+
+                else if (sum > great2) {
+                    int temp = great2;
+                    great2 = sum;
+                    great3 = temp;
+                }
+
+                else great3 = sum;
+            }
+		    sum = 0;
+	    }
         else sum += stoi(tempstr);
         getline(cin, tempstr);
     }
-    // if (sum > great1) {
-    //     int temp1 = great1;
-    //     int temp2 = great2;
-    //     great1 = sum;
-    //     great2 = temp1;
-    //     great3 = temp2;
-    // }
+
+    if (sum > great3) {
+                if (sum > great1) {
+                    int temp1 = great1;
+                    int temp2 = great2;
+                    great1 = sum;
+                    great2 = temp1;
+                    great3 = temp2;
+                }
+
+                else if (sum > great2) {
+                    int temp = great2;
+                    great2 = sum;
+                    great3 = temp;
+                }
+
+                else great3 = sum;
+            }
+
+    // cout << great1 << "\n" + great2 << "\n" << great3;
     cout << (great1 + great2 + great3);
     return 0;
 }
